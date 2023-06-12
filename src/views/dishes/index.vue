20d7bb8e4f21f4643af3ffaa83c06a92d2caf72e
bluelllblue <2179192627@qq.com>
2023-06-12 23:20:43 +0800

    lll1

--- src/views/dishes/index.vue
+++ src/views/dishes/index.vue
@@ -1,23 +1,15 @@
 <template>
-    <div>
-       
-    </div>
+  <div>啦啦啦啦啦啦啦啦啦</div>
 </template>
 <script>
-
-export default{
-    name:'Dishes', //组件名称
-    data(){
-        return{
-
-        }
-    },
-    methods:{
-
-    },
-}
+export default {
+  name: "Dishes", //组件名称
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 <!-- scoped: 作用域，当前css只当前的组件生效-->
 <style lang="less" scoped>
-
 </style>
